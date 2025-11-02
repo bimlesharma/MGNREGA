4745eb2247b0de880a93d2e868bf18750db879f3
bimlesharma <bimlesh.mdb@gmail.com>
2025-11-02 14:23:08 +0530

    import leaflet removed

--- app/map/layout.tsx
+++ app/map/layout.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function MapLayout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     // Import Leaflet CSS dynamically
-    import('leaflet/dist/leaflet.css');
+    // import('leaflet/dist/leaflet.css');
   }, []);
 
   return <>{children}</>;
